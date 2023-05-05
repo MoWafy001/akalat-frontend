b4d149a8d2831aa123e214ae5035da80e77be179
mowafy001 <wafy123445@gmail.com>
2023-05-05 21:13:58 +0300

    feat: add a footer

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -64,10 +64,44 @@ export const Layout = () => {
           </li>
         </ul>
       </nav>
-
       <div className="content">
         <Outlet />
       </div>
+
+      <footer>
+        <div className="footer-top">
+          <div className="follow-us col">
+            <h3>Follow Us</h3>
+            <div className="socials">
+              <a href="https://www.facebook.com/">
+                <img src="/facebook-logo.png" alt="facebook" />
+              </a>
+              <a href="https://www.instagram.com/">
+                <img src="/instagram.png" alt="instagram" />
+              </a>
+              <a href="https://www.twitter.com/">
+                <img src="/twitter-black-shape.png" alt="twitter" />
+              </a>
+            </div>
+          </div>
+          <div className="contact-us col">
+            <h3>Contact Us</h3>
+            <div className="contacts">
+              <div className="contact">
+                <span>Call us: </span>
+                <a href="tel:+1-279-349-5035">1-279-349-5035</a>
+              </div>
+              <div className="contact">
+                <span>Email: </span>
+                <a href="mailto:Akalat12@gmail.com">Akalat12@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="copyrights">
+          <span>Copyright © 2023,Akalat</span>
+        </div>
+      </footer>
     </>
   );
 };
