0fca00d9d52c2a0e42749a2751207ff32774f1e5
mowafy001 <wafy123445@gmail.com>
2023-05-05 20:23:40 +0300

    feat: build the nav bar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,15 +3,62 @@ import { Link, Outlet } from "react-router-dom";
 export const Layout = () => (
   <>
     <nav>
-      <ul>
+      {/* logo */}
+      <div className="nav-logo">
+        <Link to="/">
+          <img src="akalat-logo.png" alt="logo" />
+          <span>Akalat</span>
+        </Link>
+      </div>
+
+      {/* Search Bar */}
+      <div className="nav-search">
+        {/* search button icon */}
+        <button>
+          <img src="/zoomer.png" alt="search" />
+        </button>
+
+        {/* search box */}
+        <input type="text" placeholder="Search" />
+
+        {/* search filter button*/}
+        <button className="humberger-menu">
+          <img src="/levels-controls.png" alt="filter" />
+        </button>
+      </div>
+
+      {/* nav links 1 */}
+      <ul className="nav-links">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/resturants">Resturants</Link>
         </li>
         <li>
-          <Link to="/blogs">Blogs</Link>
+          <Link to="/delivery">Delivery</Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/meals">Meals</Link>
+        </li>
+      </ul>
+
+      {/* nav links 2 */}
+      <ul className="nav-links nav-right">
+        <li>
+          <Link to="/wishlist">
+            <img src="/heart-outline-shape.png" alt="wishlist" />
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/cart">
+            <img src="shopping-cart.png" alt="cart" />
+          </Link>
+        </li>
+
+        {/* hamburger menu */}
+        <li>
+          <button>
+            <img src="menu.png" alt="menu" />
+          </button>
         </li>
       </ul>
     </nav>
