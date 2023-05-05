dd3ba5f1b7e5f2007b7af5c4ccb86085f7f16fe5
mowafy001 <wafy123445@gmail.com>
2023-05-05 20:41:38 +0300

    feat: show and hide the search bar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,68 +1,73 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
-export const Layout = () => (
-  <>
-    <nav>
-      {/* logo */}
-      <div className="nav-logo">
-        <Link to="/">
-          <img src="akalat-logo.png" alt="logo" />
-          <span>Akalat</span>
-        </Link>
-      </div>
+export const Layout = () => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
 
-      {/* Search Bar */}
-      <div className="nav-search">
-        {/* search button icon */}
-        <button>
-          <img src="/zoomer.png" alt="search" />
-        </button>
-
-        {/* search box */}
-        <input type="text" placeholder="Search" />
-
-        {/* search filter button*/}
-        <button className="humberger-menu">
-          <img src="/levels-controls.png" alt="filter" />
-        </button>
-      </div>
-
-      {/* nav links 1 */}
-      <ul className="nav-links">
-        <li>
-          <Link to="/resturants">Resturants</Link>
-        </li>
-        <li>
-          <Link to="/delivery">Delivery</Link>
-        </li>
-        <li>
-          <Link to="/meals">Meals</Link>
-        </li>
-      </ul>
-
-      {/* nav links 2 */}
-      <ul className="nav-links nav-right">
-        <li>
-          <Link to="/wishlist">
-            <img src="/heart-outline-shape.png" alt="wishlist" />
+  return (
+    <>
+      <nav>
+        {/* logo */}
+        <div className="nav-logo">
+          <Link to="/">
+            <img src="akalat-logo.png" alt="logo" />
+            <span>Akalat</span>
           </Link>
-        </li>
+        </div>
 
-        <li>
-          <Link to="/cart">
-            <img src="shopping-cart.png" alt="cart" />
-          </Link>
-        </li>
+        {isHome && (
+          <div className="nav-search">
+            <button>
+              <img src="/zoomer.png" alt="search" />
+            </button>
 
-        {/* hamburger menu */}
-        <li>
-          <button>
-            <img src="menu.png" alt="menu" />
-          </button>
-        </li>
-      </ul>
-    </nav>
+            <input type="text" placeholder="Search" />
 
-    <Outlet />
-  </>
-);
+            <button className="humberger-menu">
+              <img src="/levels-controls.png" alt="filter" />
+            </button>
+          </div>
+        )}
+
+        {/* nav links 1 */}
+        <ul className="nav-links">
+          <li>
+            <Link to="/resturants">Resturants</Link>
+          </li>
+          <li>
+            <Link to="/delivery">Delivery</Link>
+          </li>
+          <li>
+            <Link to="/meals">Meals</Link>
+          </li>
+        </ul>
+
+        {/* nav links 2 */}
+        <ul className="nav-links nav-right">
+          <li>
+            <Link to="/wishlist">
+              <img src="/heart-outline-shape.png" alt="wishlist" />
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/cart">
+              <img src="shopping-cart.png" alt="cart" />
+            </Link>
+          </li>
+
+          {/* hamburger menu */}
+          <li>
+            <button>
+              <img src="menu.png" alt="menu" />
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="content">
+        <Outlet />
+      </div>
+    </>
+  );
+};
